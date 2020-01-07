fa482e46ed72e97eb5e8caec78148fc5799b6ea5
denia.dobras <dobras.denia@gmail.com>
2020-01-07 15:53:12 +0200

    deleted files do not merge

--- Part3_exercise1.py
+++
@@ -1,9 +0,0 @@
-
-minimum = int(input("Enter the start of range: "))
-maximum = int(input("Enter the end of range: "))
-amount = int(input("Enter the range: "))
-
-
-for i in range(minimum, maximum + 1, amount):
-    print(i, end=' ')
-
--- Part3_exercise2.py
+++
@@ -1,6 +0,0 @@
-#Exercise2 - print something backwards
-
-message = input("Tell me something: ")
-
-print(message[::-1])
-
